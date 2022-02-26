8e8dbcd8171f316facc1f189506a30130642e297
Md Rafi <mdradmihanchyrafi@gmail.com>
2022-02-26 10:24:16 +0600

    tut-23 components structure for newsapp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c='harry';
-  render() {
-    
+  // c='harry';
+  render() { 
     return (
-      <div>Hello my first app {this.c}</div>
+      // <div>Hello my first app {this.c}</div>
+      <div>
+        <NavBar/>
+        <News/>
+      </div>
     )
   }
 }
