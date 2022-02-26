0590872cb68ec9a39d71e5546c3972ffa4653073
Md Rafi <mdradmihanchyrafi@gmail.com>
2022-02-26 09:59:06 +0600

    tut-22 setup pro-2 NewsApp using class based component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c='harry';
+  render() {
+    
+    return (
+      <div>Hello my first app {this.c}</div>
+    )
+  }
+}
