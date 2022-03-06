0779ea96ac93272978e501975080fee0309df2c2
Md Rafi <mdradmihanchyrafi@gmail.com>
2022-03-06 22:18:22 +0600

    tut-27 using fetch api

--- src/components/News.js
+++ src/components/News.js
@@ -2,50 +2,33 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles = [
-    {
-    "source": {
-    "id": "espn-cric-info",
-    "name": "ESPN Cric Info"
-    },
-    "author": null,
-    "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-    "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-    "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-    "publishedAt": "2020-04-27T11:41:47Z",
-    "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-    "source": {
-    "id": "espn-cric-info",
-    "name": "ESPN Cric Info"
-    },
-    "author": null,
-    "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-    "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-    "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-    "publishedAt": "2020-03-30T15:26:05Z",
-    "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-    ]
   constructor(){
     super();
     console.log('hello I am a constructor from news component');
     this.state={
-      articles:this.articles,
+      articles:[],
       loading:false,
     }
   }
+
+  async componentDidMount(){
+    console.log('cdn');
+    let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=4502b5e1b10b488f99a4abc72f717a0b";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({articles:parsedData.articles});
+  }
+
   render() {
+    console.log('render');
     return (
       <div className='container my-3'>
-          <h3>NewsMonkey - Top Headlines</h3>
-          <div className="row my-2">
+          <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+          <div className="row my-3">
           {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
-            <NewsItem title={element.title} description={element.description.slice(0,100)} imageUrl={element.urlToImage}
+            <NewsItem title={element.title?element.title.slice(0,60):"unknown"} description={element.description?element.description.slice(0,100):'description'} imageUrl={element.urlToImage}
             newsUrl={element.url}/>
             </div>
           })}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,9 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={imageUrl?imageUrl:"https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/3/2022_3$largeimg_1025308223.JPG"} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
                 <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read more..</a>
             </div>
